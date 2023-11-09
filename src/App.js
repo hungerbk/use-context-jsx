17f582db2357d05a07d355a8cd63f5d7368b8734
hungerbk <wjantlr@gmail.com>
2023-11-10 00:00:22 +0900

    zustand 학습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import "./App.css";
 import Page from "./components/Page";
+import useStore from "./store";
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
-  return <Page isDark={isDark} toggleThema={() => setIsDark(!isDark)} />;
+  const isDark = useStore((state) => state.isDark);
+  const setIsDark = useStore((state) => state.setIsDark);
+  return <Page isDark={isDark} toggleThema={setIsDark} />;
 }
 
 export default App;
